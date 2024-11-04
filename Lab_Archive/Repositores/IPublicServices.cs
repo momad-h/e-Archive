67b9a70bcb28082a8ce13ce61b88bfb32966bbcc
Momad <s_lpln@yahoo.com>
2024-11-04 13:39:29 +0330

    Rearrange the Projects

---
+++ Lab_Archive/Repositores/IPublicServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Archive.Repositores
+{
+    internal interface IPublicServices
+    {
+        bool Login();
+        bool Logout();
+        void LoadConfig();
+    }
+}
